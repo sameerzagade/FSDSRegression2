f7f9c07d42939377dd15a4eb37d2deaf72950750
sameer zagade <sameerzagade1985@gmail.com>
2023-06-04 23:31:30 +0530

    app.py and web application

--- src/utils.py
+++ src/utils.py
@@ -51,4 +51,10 @@ def load_object(file_path):
         logging.info('Exception Occured in load_object function utils')
         raise CustomException(e,sys)
 
-    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_objecct function until')
+        raise CustomException(e,sys)
